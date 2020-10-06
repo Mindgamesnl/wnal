64cb355eb174823a53de361a915568554f6f0967
Mindgamesnl <matsmoolhuizen@gmail.com>
2020-10-07 01:24:06 +0200

    web shit

---
+++ webclient/src/js/Wnal.js
@@ -0,0 +1,6 @@
+import "@babel/polyfill";
+import {Terminal} from "xterm";
+
+var term = new Terminal();
+term.open(document.getElementById('terminal'));
+term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
---
+++ webclient/unix-build.sh
@@ -0,0 +1,7 @@
+rm -rf build/
+mkdir build/
+npm run-script build
+cp -r src/assets* build/
+cp -r src/css* build/
+cp -r src/index.html build/
+cp dist/Wnal.bundle.js build/
